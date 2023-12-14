074b239e1bc6aef4898f87514752dde233f2fd90
thoongnt <thong231203@gmail.com>
2023-12-15 02:01:48 +0700

    commit lại do chưa lưu

--- subway-suffer-client/Assets/Scripts/OtherPage/DeactiveMessageBox.cs
+++ subway-suffer-client/Assets/Scripts/OtherPage/DeactiveMessageBox.cs
@@ -8,8 +8,23 @@ public class DeactiveMessageBox : MonoBehaviour {
         transform.gameObject.SetActive(false);
     }
 
+    public void ButtonCloseMessage()
+    {
+        Modules.PlayAudioClipFree(Modules.audioButton);
+        transform.gameObject.GetComponent<Animator>().SetTrigger("TriClose");
+    }
+
     public void PlayAudioShow()
     {
         Modules.PlayAudioClipFree(Modules.audioShowMessage);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (transform.gameObject.activeSelf)
+                ButtonCloseMessage();
+        }
     }
 }
