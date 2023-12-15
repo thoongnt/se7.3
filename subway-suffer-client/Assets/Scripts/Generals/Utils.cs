c999c95ea5a5e1e75c067748e5079992c1fbfd4d
ThanhTranDz <tranthanh181103@gmail.com>
2023-12-16 01:02:08 +0700

    Log in FB

---
+++ subway-suffer-client/Assets/Scripts/Generals/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.Generals
+{
+    public static class Utils
+    {
+        public static IEnumerator LoadImage(string url)
+        {
+            using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url))
+            {
+                yield return uwr.SendWebRequest();
+
+                if (uwr.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.Log(uwr.error);
+                }
+                else
+                {
+                    // Get downloaded asset bundle
+                    var texture = DownloadHandlerTexture.GetContent(uwr);
+                }
+            }
+        }
+    }
+}
