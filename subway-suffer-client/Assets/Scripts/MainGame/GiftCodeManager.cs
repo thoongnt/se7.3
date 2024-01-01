736cd891c571122c4968c75c9eb5d6c9086a37cf
thoongnt <thong231203@gmail.com>
2024-01-01 15:24:17 +0700

    thêm list chứa giftcode và hashset chứa các giftcode đã được sử dụng và tắt feedbacktext sau 3s

--- subway-suffer-client/Assets/Scripts/MainGame/GiftCodeManager.cs
+++ subway-suffer-client/Assets/Scripts/MainGame/GiftCodeManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class GiftCodeManager : MonoBehaviour
 {
@@ -10,30 +11,51 @@ public class GiftCodeManager : MonoBehaviour
 
     private int valueGiftCodeCoin = 100000;
     private int valueGiftCodeKey = 100;
+    private List<string> giftCodes = new List<string>();
+    private HashSet<string> usedGiftCodes = new HashSet<string>();
 
     void Start()
     {
+        AddGiftCodes();
         Button btn = submitButton.GetComponent<Button>();
         btn.onClick.AddListener(OnSubmitButtonClicked);
+    }
+
+    void AddGiftCodes()
+    {
+        giftCodes.Add("GIFT123");
+        giftCodes.Add("GIFT456");
     }
 
     public void OnSubmitButtonClicked()
     {
         string enteredCode = codeInputField.text;
 
-        if (ProcessGiftCode(enteredCode))
+        if (usedGiftCodes.Contains(enteredCode))
         {
-            feedbackText.text = "Gift code is valid! Congratulations!";
+            feedbackText.text = "Gift code has already been used!";
         }
         else
         {
-            feedbackText.text = "Invalid gift code! Please try again.";
+            if (ProcessGiftCode(enteredCode))
+            {
+                usedGiftCodes.Add(enteredCode);
+                feedbackText.text = "Gift code is valid! Congratulations!";
+
+                Invoke(nameof(ClearFeedbackText), 3f);
+            }
+            else
+            {
+                feedbackText.text = "Invalid gift code! Please try again.";
+                
+                Invoke(nameof(ClearFeedbackText), 3f);
+            }
         }
     }
 
     bool ProcessGiftCode(string code)
     {
-        if (code == "GIFT123")
+        if (giftCodes.Contains(code))
         {
             AwardCoin();
             AwardKey();
@@ -50,5 +72,11 @@ public class GiftCodeManager : MonoBehaviour
     void AwardKey()
     {
         Modules.GiftCodeKey(valueGiftCodeKey);
+    }
+
+    
+    void ClearFeedbackText()
+    {
+        feedbackText.text = "";
     }
 }
